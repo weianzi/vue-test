01017095c262000cf60643d037156b74f29ad64f
weianzi <ancool@foxmail.com>
2019-10-20 15:55:04 +0800

    'b'

---
+++ www/precache-manifest.66981fefabee7884bc9d046115c0d1ee.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "80e47055aa7161eb1450",
+    "url": "static/css/table.084bc15e.css"
+  },
+  {
+    "revision": "e58579af761522d9301f",
+    "url": "static/js/about.415757f0.js"
+  },
+  {
+    "revision": "b4d9355d4a9da753aea7",
+    "url": "static/js/app.1794519a.js"
+  },
+  {
+    "revision": "80e47055aa7161eb1450",
+    "url": "static/js/table.b0781f9c.js"
+  },
+  {
+    "revision": "51d29702f53aaa5ca714",
+    "url": "static/js/chart.d71b1f38.js"
+  },
+  {
+    "revision": "b8c1df5d8eac4d64b70a",
+    "url": "static/js/chunk-vendors.5ac380b9.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "static/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "static/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "b8c1df5d8eac4d64b70a",
+    "url": "static/css/chunk-vendors.590d32fe.css"
+  },
+  {
+    "revision": "51d29702f53aaa5ca714",
+    "url": "static/css/chart.b7db458f.css"
+  },
+  {
+    "revision": "b4d9355d4a9da753aea7",
+    "url": "static/css/app.f15fb4e4.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "41590cc2233b182737a7b999c868782b",
+    "url": "index.html"
+  }
+];
