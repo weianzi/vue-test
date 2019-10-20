c2178a734284fb7573674b3b6349ee28e06f7942
weianzi <ancool@foxmail.com>
2019-10-20 10:29:17 +0800

    'high'

--- src/views/Chart/Ring.vue
+++ src/views/Chart/Ring.vue
@@ -10,7 +10,7 @@
 <script>
 import echarts from 'echarts'
 import resize from '@/components/Charts/mixins/resize'
-
+import DATA from './mock/dataRing.json'
 export default {
   mixins: [resize],
   data() {
@@ -19,14 +19,7 @@ export default {
       option
     }
   },
-  computed: {
-    user() {
-      return this.$store.state.user
-    }
-  },
-  async mounted() {
-    await this.$store.dispatch('getUserInfo')
-    //console.log(this.user.name)
+  mounted() {
     this.initChart()
   },
   beforeDestroy() {
@@ -38,206 +31,256 @@ export default {
   },
   methods: {
     initChart() {
-      this.chart = echarts.init(document.getElementById('chart'))
+      this.chart = echarts.init(document.getElementById('chart'), null, {
+        renderer: 'svg'
+      })
       this.chart.setOption(this.option)
 
-      this.chart.on('click', param => {
-        console.log(param)
-        this.option.series.levels = [
-          [
-            {},
-            {
-              r0: '35%',
-              r: '45%'
-            },
-            {
-              r0: '50%',
-              r: '55%'
-            },
-            {
-              r0: '55%',
-              r: '60%'
-            }
-          ],
-          [
-            {},
-            {
-              r0: '35%',
-              r: '40%'
-            },
-            {
-              r0: '40%',
-              r: '50%'
-            },
-            {
-              r0: '55%',
-              r: '60%'
-            }
-          ],
-          [
-            {},
-            {
-              r0: '35%',
-              r: '40%'
-            },
-            {
-              r0: '40%',
-              r: '45%'
-            },
-            {
-              r0: '45%',
-              r: '55%'
-            }
-          ]
-        ][param.treePathInfo.length - 2]
-        this.option.series.startAngle = '' + param.data.deg
-        this.chart.setOption(this.option)
+      // this.chart.dispatchAction({
+      //   type: 'highlight',
+      //   seriesIndex: 0,
+      //   dataIndex: 80
+      // })
+
+      //console.log(this.option.series.data)
+
+      var name = ''
+      // this.chart.on('click', params => {
+      //   console.log(params)
+      // })
+      this.chart.on('click', params => {
+        name = params.data.name
+        name &&
+          this.chart.dispatchAction({
+            type: 'highlight',
+            name
+          }) //选中高亮
+
+        // DATA.forEach((item, index) => {
+        //   item.children.forEach(child => {
+        //     console.log(child.name)
+        //     if (child.name != name) {
+        //       this.chart.dispatchAction({
+        //         type: 'downplay',
+        //         //seriesIndex: 0,
+        //         name: child.name
+        //       }) //取消高亮
+        //     }
+        //   })
+        // })
+        // for (var i = 0; i < DATA.length; i++) {
+        //   if (DATA[i].name == name) {
+        //     this.chart.dispatchAction({
+        //       type: 'downplay',
+        //       seriesIndex: 0,
+        //       dataIndex: i
+        //     }) //取消高亮
+        //   }
+        // }
+
+        console.log(params, name)
       })
+      this.chart.on('mouseout', params => {
+        name &&
+          this.chart.dispatchAction({
+            type: 'highlight',
+            name
+          })
+        // this.chart.dispatchAction({
+        //   type: 'downplay',
+        //   name: DATA[0].name
+        // })
+      })
+      // this.chart.on('mouseout', params => {
+      //   // console.log(params)
+      //   console.log(index)
+      //   this.chart.dispatchAction({
+      //     type: 'highlight',
+      //     seriesIndex: 0,
+      //     dataIndex: index
+      //   }) //选中高亮
+      //   for (var i = 0; i < DATA.length; i++) {
+      //     console.log(DATA[i].name, name)
+      //     if (DATA[i].name !== name) {
+      //       this.chart.dispatchAction({
+      //         type: 'downplay',
+      //         seriesIndex: 0,
+      //         dataIndex: i
+      //       }) //取消高亮
+      //     }
+      //   }
+      // })
     }
   }
 }
 
-const temp = [
-  {
-    name: 'a',
-    value: 1,
-    deg: 0 + 15,
-    children: [
-      {
-        deg: 15,
-        children: [
-          {
-            deg: 15,
-            value: 1
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'b',
-    value: 1,
-    deg: 45,
-    children: [
-      {
-        deg: 45,
-        children: [
-          {
-            deg: 45,
-            value: 1
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'c',
-    value: 1,
-    deg: 75,
-    children: [
-      {
-        deg: 75,
-        children: [
-          {
-            deg: 75,
-            value: 1
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'd',
-    value: 6,
-    deg: 90 + 90,
-    children: [
-      {
-        deg: 60 + 90,
-        value: 4,
-        children: [
-          {
-            deg: 30 + 90,
-            value: 2
-          },
-          {
-            deg: 15 + 90 + 60,
-            value: 1
-          },
-          {
-            deg: 15 + 90 + 60 + 30,
-            value: 1
-          }
-        ]
-      },
-      {
-        deg: 30 + 120 + 90,
-        value: 2
-      }
-    ]
-  },
-  {
-    name: 'e',
-    value: 3,
-    deg: 90 + 6 * 30 + 3 * 15,
-    children: [
-      {
-        deg: 90 + 6 * 30 + 2 * 15,
-        children: [
-          {
-            deg: 90 + 6 * 30 + 2 * 15,
-            value: 2
-          }
-        ]
-      }
-    ]
-  }
-]
-
 const option = {
-  tooltip: {
-    trigger: 'item',
-    formatter: "{a} <br/>{b}: {c}"
-  },
+  backgroundColor: '#000',
   series: {
-    startAngle: '15',
-    // radius: ['50%', '95%'],
+    radius: ['28%', '80%'],
     center: ['50%', '50%'],
     type: 'sunburst',
     sort: null,
-    nodeClick: false,
-    data: temp,
-    levels: [
-      {},
-      {
-        r0: '35%',
-        r: '45%'
-      },
-      {
-        r0: '50%',
-        r: '55%'
-      },
-      {
-        r0: '55%',
-        r: '60%'
-      }
-    ],
-    label: {
-      normal: {
-        show: false,
-        rotate: 'none',
-        color: '#fff'
+    z: 3,
+    tooltip: {
+      formatter: function(params) {
+        let html =
+          '<div class="index_model">' +
+          '<h3 class="index_model_h3">' +
+          params.name +
+          '</h3>' +
+          '<div class="index_model_rate">' +
+          params.value +
+          '%<span>' +
+          '控股' +
+          '</span></div>' +
+          '<div class="index_model_proportion">' +
+          '企业股权占比' +
+          '</div><div style="width:70%;padding:0.04rem 0;">' +
+          '全资子企业' +
+          '<span style="float:right;">' +
+          '7 家' +
+          '</span></div><div style="width:70%;padding:0.04rem 0;">' +
+          '控股子企业' +
+          '<span style="float:right;">' +
+          '8家' +
+          '</span></div></div>'
+        if (params.name == '') {
+          return ''
+        } else {
+          return html
+        }
       }
     },
+    //allowDrillToNode: true,
+    //nodeClick: false,
+    cursor: 'pointer',
+    highlightPolicy: 'self',
+    label: { show: false },
     itemStyle: {
-      color: '#4581da',
-      borderWidth: 3,
-      borderColor: '#f3f3f3'
+      color: {
+        type: 'linear',
+        x: 0,
+        y: 0,
+        x2: 0,
+        y2: 1,
+        colorStops: [
+          {
+            offset: 0,
+            color: '#36b1ff' // 0% 处的颜色
+          },
+          {
+            offset: 1,
+            color: '#72a4ff' // 100% 处的颜色
+          }
+        ],
+        global: false // 缺省为 false
+      },
+      opacity: 0.6,
+      borderWidth: 2,
+      borderColor: 'rgba(0, 0, 0, 0.1)'
+    },
+    highlight: {
+      itemStyle: {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: '#36b1ff' // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: '#72a4ff' // 100% 处的颜色
+            }
+          ],
+          global: false // 缺省为 false
+        },
+        opacity: 1
+      }
     },
     emphasis: {
       itemStyle: {
-        color: '#4ea6e5'
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: '#36b1ff' // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: '#72a4ff' // 100% 处的颜色
+            }
+          ],
+          global: false // 缺省为 false
+        },
+        opacity: 1
       }
-    }
+    },
+    downplay: {
+      itemStyle: {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: '#36b1ff' // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: '#72a4ff' // 100% 处的颜色
+            }
+          ],
+          global: false // 缺省为 false
+        },
+        opacity: 0.4
+      }
+    },
+    //外圆
+    levels: [
+      {
+        // itemStyle: {
+        //   opacity: 0
+        // },
+        r0: '1%',
+        r: '2%',
+      },
+      {
+        r0: '38%',
+        r: '47%',
+
+      },
+      {
+        r0: '52%',
+        r: '57%'
+      },
+      {
+        r0: '60%',
+        r: '64%'
+      },
+      {
+        r0: '67%',
+        r: '71%'
+      },
+      {
+        r0: '73%',
+        r: '75.5%'
+      }
+    ],
+    data: DATA
   }
 }
 </script>
