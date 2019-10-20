560cd74abc6fc5c571aded3503b31641e53027f1
weianzi <ancool@foxmail.com>
2019-10-20 15:50:18 +0800

    'b'

--- Vue.config.js
+++ Vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  productionSourceMap: false,
   assetsDir: './static',
   outputDir: 'www',
   publicPath: process.env.NODE_ENV == 'production' ? './' : '' //打包时，html引入资源以 ./ 开头
